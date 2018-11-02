9e26f2f423caebb3e5aace5de70ba290ccee562b
brennx0r <brennx0r@gmail.com>
2018-11-01 21:52:50 -0700

    Add image, the full array for prompts. Later version will use Oxford dictionary API.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,85 @@ var router = express.Router();
 function generatePromptWords() {
   
   randwordArray = []
-  wordArray = ["ambitous", "over-reaching", "forkdoor", "slaw", "mayo", "winner"]
+
+  wordArray = [
+    "ambitous", 
+    "paradise", 
+    "forkdoor", 
+    "hope",
+    "green",
+    "overgrown",
+    "icy",
+    "festive",
+    "complicated",
+    "morose",
+    "glittering",
+    "bounty",
+    "thankful",
+    "serene",
+    "challenging",
+    "shocked",
+    "basic",
+    "flowering",
+    "spacious",
+    "grace",
+    "slytherin",
+    "muppet",
+    "cuddly",
+    "alien",
+    "swamp",
+    "rubies",
+    "clammy",
+    "vacation",
+    "romantic",
+    "cookie cat",
+    "sweeping",
+    "drowning",
+    "pillow",
+    "smooshed",
+    "trapped",
+    "verdant",
+    "skull",
+    "piece of cake",
+    "swallow",
+    "hibernate",
+    "bark",
+    "furry",
+    "double",
+    "retired",
+    "fashionable",
+    "gleaming",
+    "lit",
+    "smitten",
+    "delicious",
+    "strong",
+    "compact",
+    "timid",
+    "miniature",
+    "glass",
+    "metallic",
+    "cozy",
+    "placid",
+    "snowy",
+    "festive",
+    "forgetful",
+    "anxious",
+    "frantic",
+    "experimental",
+    "testing",
+    "work",
+    "play",
+    "gaming",
+    "travel",
+    "election",
+    "cast",
+    "flip",
+    "sway",
+    "dance",
+    "reflecting",
+    "end",
+  ]
+
   var day;
 
   for (day = 1; day < 31; day++) {
@@ -16,15 +94,12 @@ function generatePromptWords() {
   
 }
 
-function generateList() {
-  return generatePromptWords()
-}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Huevember Prompt-word and Color Creator',
-    words: generateList()
+    words: generatePromptWords()
  });
 
 });
